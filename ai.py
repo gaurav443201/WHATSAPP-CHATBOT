ed4519f33488bdc2047f6cc50fe10f0a648d5a62
CRAZYSHIT <NAVGHAREGAURAV80@GMAIL.COM>
2025-07-17 19:03:23 +0530

    ai.py

--- ai.py
+++ ai.py
@@ -1,6 +1,6 @@
 from together import Together
 
-client = Together(api_key="67f4b43be85bc61720e44a42d4c5b6cf26904245e11f85a9bffba0c4506d113a") # auth defaults to os.environ.get("TOGETHER_API_KEY")
+client = Together(api_key="") # auth defaults to os.environ.get("TOGETHER_API_KEY")
 
 response = client.chat.completions.create(
     model="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
